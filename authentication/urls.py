e257ad99dd9f6d296ec1f4e3f0bb6f484cb315b7
zarif007 <zarifhuq786@gmail.com>
2021-03-02 00:01:27 +0600

    Preparing register form.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import RegistrationView, UsernameValidation
+from .views import RegistrationView, UsernameValidation, EmailValidation, PasswordValidation
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
     path('validate-username', csrf_exempt(UsernameValidation.as_view()), 
                                 name='validate_username'),
+    path('validate-email', csrf_exempt(EmailValidation.as_view()), 
+                                name='validate_email'),
+    path('validate-password', csrf_exempt(PasswordValidation.as_view()), 
+                                name='validate_password'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
+from password_validator import PasswordValidator
 
 
 class UsernameValidation(View):
@@ -19,6 +21,39 @@ class UsernameValidation(View):
                                 status=400)
         
         return JsonResponse({'username_valid': True})
+
+
+class EmailValidation(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'not a valid email address'}, 
+                                status=400)
+
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'email in use, You can log in with this email'}, 
+                                status=400)
+        
+        return JsonResponse({'email_valid': True})
+
+
+class PasswordValidation(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        password = data['password']
+
+        schema = PasswordValidator()
+        schema.min(8).max(100).has().uppercase().has().lowercase().has().digits()
+
+        if not schema.validate(password):
+            return JsonResponse({'password_error': 'not a valid password(At least 8 charecter, \
+                                                    both upper and lower case, must contains digits)'}, 
+                                status=400)
+
+        
+        return JsonResponse({'password_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
